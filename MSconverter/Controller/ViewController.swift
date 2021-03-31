b9b281b97721616426c918fa7daa34be25cc2e0f
ErlingFX <75131227+ErlingFX@users.noreply.github.com>
2021-04-01 00:43:44 +0400

    Create ViewController.swift

---
+++ MSconverter/Controller/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  MSconverter
+//
+//  Created by Alexander Nazarov on 31.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        
+    }
+
+
+}
+
